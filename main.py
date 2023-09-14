bad97ddf5aef1d91e406321d2f1aa5ed451aa972
JericoI <144091976+JericoI@users.noreply.github.com>
2023-09-14 12:50:12 -0700

    First Upload
    Downloaded from Replit

---
+++ main.py
@@ -0,0 +1,10 @@
+l = int(input("Length?\n"))
+w = int(input("Width?\n"))
+h = int(input("Height\n"))
+
+flap1_l = w/2
+flap1_w = l/2
+
+print(f"\nLength: {l}\nWidth: {w}\nHeight: {h}")
+print(f"\nThe first and third panel's lengths are {l} and the height is {h}. The flaps are {flap1_l} high.")
+print(f"The second and fourth panel's lengths are {w} and the height is {h}. The flaps are {flap1_w} high.")
